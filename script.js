4655de859d2730ec380f06feadb0359402f7aefe
Gibson Ainge <gainge@gmail.com>
2019-06-08 09:17:08 -0600

    Draft 1 of the caption area... might switch to a div or something

---
+++ script.js
@@ -0,0 +1,71 @@
+
+// Constants
+const ADD = "+";
+const DELETE = "-";
+
+const DEFAULT_FONT_SIZE = "58px";
+
+
+
+
+function isScrolling(el) {  
+  return el.scrollHeight > el.clientHeight;
+}
+
+document.getElementById("caption1").style.fontSize = DEFAULT_FONT_SIZE;
+
+document.getElementById("caption1").oninput = () => {
+  var el = document.getElementById("caption1");
+
+  if (isScrolling(el)) {
+    removeScrolling(el);
+  } else {
+    tryFontGrowth(el);
+  }
+}
+
+
+
+
+
+
+// Helper Methods
+function getFontSizeInt(el) {
+  return parseInt(el.style.fontSize);
+}
+
+
+function tryFontGrowth(el) {
+  if (el.style.fontSize === DEFAULT_FONT_SIZE) return;
+
+  var fontSize = getFontSizeInt(el);
+  var maxFont = parseInt(DEFAULT_FONT_SIZE);
+
+  // So now we just try to scroll
+  while (fontSize < maxFont) {
+    fontSize++;
+    el.style.fontSize = fontSize + "px";
+
+    if (isScrolling(el)) {
+      fontSize--;
+      el.style.fontSize = fontSize + "px";
+      break;
+    }
+  }
+}
+
+
+function removeScrolling(el) {
+  var fontSize = getFontSizeInt(el);
+
+  while (isScrolling(el) && fontSize > 1) {
+    fontSize--;
+    el.style.fontSize = fontSize + "px";
+  }
+}
+
+
+function addControlButton(parentElement, action, position) {
+  // This is where I'd like, curry out an action function based on the position?
+  // I think I like that style :)
+}
